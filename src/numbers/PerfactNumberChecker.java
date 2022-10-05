f92ad0604fc642157cd2acd98b8a018982416b12
alagu-p <alagappan.plr@gmail.com>
2022-10-05 22:08:47 +0530

    oct-5

--- src/numbers/PerfactNumberChecker.java
+++ src/numbers/PerfactNumberChecker.java
@@ -1,9 +1,34 @@
 package numbers;
 
-public class PerfactNumberChecker {
+import java.util.Scanner;
 
+public class PerfactNumberChecker {
+/*
+ *  6
+ *  6%i==> 1+2+3=6
+ *  1+2+4+7+14=28
+ *  
+ */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter the Number : ");
+		int number=sc.nextInt();
+		int sum=0;
+		
+		for(int i=1;i<=(number/2);i++)
+		{
+			if(number%i==0)
+				sum+=i;
+				
+		}
+		if(sum==number)
+			System.out.println("Perfact Number...");
+		else
+			System.out.println("Not Perfact Number....");
+		sc.close();
+		
 
 	}
 
