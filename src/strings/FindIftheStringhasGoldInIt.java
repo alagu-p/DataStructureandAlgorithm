2bff3da14ba20eb7d470b058e3158559d53fb904
alagu-p <alagappan.plr@gmail.com>
2022-10-07 06:45:58 +0530

    Oct -7 String Continuetee

---
+++ src/strings/FindIftheStringhasGoldInIt.java
@@ -0,0 +1,43 @@
+package strings;
+
+import java.util.Scanner;
+
+public class FindIftheStringhasGoldInIt {
+
+	
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the String : ");
+		String text=sc.nextLine(),s="gold";
+		
+		text=text.toLowerCase();
+		boolean found=false;
+		for(int i=0;i<text.length();i++)
+		{
+			//k is PlaceHolder
+			int k=i,j;
+			for( j=0;j<s.length();j++)
+			{
+				if(text.charAt(k) !=s.charAt(j))
+				{
+					break;
+				}
+				k++;	
+			}
+			if(j==s.length()) {
+				found=true;
+				break;
+			}
+		}
+		if(found)
+			System.out.println("Yapp... Gold Found.");
+		else
+			System.out.println("Nopp... Gold not found.");
+		
+		
+		sc.close();
+
+	}
+
+}
--- src/strings/FindMaxOccurences.java
+++ src/strings/FindMaxOccurences.java
@@ -1,10 +1,34 @@
 package strings;
 
+import java.util.Scanner;
+
 public class FindMaxOccurences {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the String : ");
+		String text=sc.nextLine();
+		int arr[] =new int[256];		
+		for(int i=0;i<text.length();i++)
+		{
+			arr[(int)text.charAt(i)]++;
+	
+		}
+		int max=0;
+		for(int j=0;j<arr.length;j++)
+		{
+			if(arr[j]>arr[max])
+			{
+				max=j;
+				
+			}
+		}
+		System.out.println("Max occuring character is : "+(char)max+" : "+arr[max]);
+		
+		
+		sc.close();
+		
 	}
 
 }
