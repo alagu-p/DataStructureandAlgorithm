84500fc0e71d0d8e4945d0687e42b1c5c20b12a9
alagu-p <alagappan.plr@gmail.com>
2022-10-10 22:21:25 +0530

    String Encoder

--- src/strings/StringEncoder.java
+++ src/strings/StringEncoder.java
@@ -9,7 +9,18 @@ public class StringEncoder {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter a String : ");
 		String text=sc.nextLine();
-		
+		String s="";
+		//method -1
+		System.out.print("Encode String : ");
+		for(int i=0;i<text.length();i++)
+		{
+			char temp=(char) (text.charAt(i)+1);
+			s=s+(char)(text.charAt(i)+1);
+			System.out.print(temp);
+		}
+		//method -2
+		System.out.println("\nEncode String : "+s);
+
 		sc.close();
 	}
 
